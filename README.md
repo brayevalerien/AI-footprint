940be047957dfe80df1583c9e8536c82a6a5fd68
Valérien <60972074+brayevalerien@users.noreply.github.com>
2024-01-15 01:51:50 +0100

    first iteration

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # AI-footprint
- A small app that computes the CO2 emissions of your AI algorithm.
+A small app that computes the CO$_2$ emissions of your AI algorithm.
+
+## Set up
+This application uses [Streamlit](https://streamlit.io). Make sure you have it installed on your computer or run the following command (more info [here](https://docs.streamlit.io/get-started/installation)):
+```bash
+pip install streamlit
+```
+To download and run AI-footprint, run:
+```bash
+git clone https://github.com/brayevalerien/AI-footpring.git
+cd AI-footprint
+streamlit run ./src/aifootprint.py
+```
+It will start the local server, that you can access by following the link provided by Streamlit. Note that once the application has been download, only the last command is necessary.
+
+## Usage
+To get an estimation of the CO$_2$ emissions of your model, fill out the form in the side bar and click on the "Submit" button. If your device model or server region is not available, choose the closest option to get the best estimation possible.
+
+## References
+The method used to compute CO$_2$ emissions is based on the following the works:
+- AI Index Report 2023 ; Artificial Intelligence Index — aiindex.stanford.edu. https://aiindex.stanford.edu/report/.
+- Green Algorithms — calculator.green-algorithms.org. https://calculator.green-algorithms.org
+- Joséphine Boone. L’IA pointée du doigt pour son coût environnemental astronomique — lesechos.fr. https://www.lesechos.fr/tech-medias/
+intelligence-artificielle/lia-pointee-du-doigt-pour-son-cout-environnemental-astronomique-1913779, 03 2023.
+- France Direction Générale des entreprises (2014-. . . . ). Intelligence artificielle, état de l’art et perspectives pour la France : rapport final. 2019.
+- Jesse Dodge, Taylor Prewitt, Remi Tachet Des Combes, Erika Odmark, Roy Schwartz, Emma Strubell, Alexandra Sasha Luccioni, Noah A. Smith, Nicole DeCario, and Will
+Buchanan. Measuring the carbon intensity of ai in cloud instances, 2022
+- Ralf Gitzel. Estimating the sustainability of ai models based on theoretical models and experimental data, 04 2023.
+- INSEE. Définition - Intensité en carbone. https://www.insee.fr/fr/metadonnees/definition/c1269, 2020.
+- Luiz Carlos De Jesus Junior. How to calculate the carbon footprint of training/running a large AI model in the cloud.
+https://blog.revolve.team/2023/06/27/ai-carbon-footprint/
+- Anne-Laure Ligozat, Julien Lefevre, Aurélie Bugeau, and Jacques Combaz. Unraveling the hidden environmental impacts of ai solutions for environment life cycle
+assessment of ai solutions. Sustainability, 14(9) :5172, April 2022.
+- David Patterson, Joseph Gonzalez, Urs Hölzle, Quoc Le, Chen Liang, Lluis-Miquel Munguia, Daniel Rothchild, David R. So, Maud Texier, and Jeff Dean. The carbon
+footprint of machine learning training will plateau, then shrink. Computer, 55(7) :18–28, 2022.
+- Rajkumar P.V. Gauging carbon footprint of ai/ml implementations in smart cities : Methods and challenges, 2022.
+- Anshul Roy. Introduction to CRISP DM Framework for Data Science and Machine Learning.
+https://www.linkedin.com/pulse/chapter-1-introduction-crisp-dm-framework-data-science-anshul-roy/, juin 2018.
+- Emma Strubell, Ananya Ganesh, and Andrew McCallum. Energy and policy considerations for deep learning in NLP. CoRR, abs/1906.02243, 2019.
---
+++ src/aifootprint.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import pandas as pd
+
+DATAPATH = "./data/"
+
+if __name__ == "__main__":
+    submited = False
+    
+    st.write("""
+             # AI footprint
+             ## Compute the CO2 footprint of your AI algorithm.
+             """)
+
+    data_cpu = pd.read_csv(DATAPATH + "TDP_cpu.csv")
+    data_gpu = pd.read_csv(DATAPATH + "TDP_gpu.csv")
+    device_types = ("CPU", "GPU", "Both")
+    cpu_models = {
+        "Core i3-10100": 0.0163,
+        "AMD 7552": 0.0042,
+        "Core i5-10500": 0.0108,
+        "Core i9-10900XE": 0.0165,
+        "Ryzen 5 1600": 0.0108,
+        "Xeon Gold 6148": 0.0075
+    }
+    gpu_models = {
+        "NVIDIA RTX 2080 Ti": 0.25,
+        "AMD RX480": 0.15,
+        "NVIDIA Tesla V100": 0.3,
+        "NVIDIA Tesla P4": 0.075,
+        "NVIDIA Tesla K80": 0.3,
+    }
+    cloud_providers = {
+        "Azure": 1.125
+    }
+    server_regions = {
+        "France": 51.28,
+        "Ontario": 30,
+        "California": 216.43,
+        "Kentucky": 804.75,
+        "Austria": 111.18,
+        "Taiwan": 509
+    }
+
+    with st.sidebar.form("parameters"):
+        st.write("# Model & training parameters")
+        st.write("----")
+
+        st.write("# Data")
+        data_size = st.number_input("Amount of training data stored (GB)", min_value=0)
+        storing_time = st.number_input("Data storing time (hours)", min_value=0)
+
+        st.write("# Training")
+        training_time = st.number_input("Training duration (hours)", min_value=0)
+        training_device_type = st.selectbox("Training device type", device_types)
+        training_cpu_cores = st.slider("Training number of CPU cores", 1, 32, 8)
+        training_cpu_model = st.selectbox("Training CPU model", cpu_models)
+        training_gpu = st.slider("Training number of GPUs", 1, 16, 8)
+        training_gpu_model = st.selectbox("Training GPU model", gpu_models)
+        training_ram = st.slider("Training RAM (GB)", 1, 64, 16)
+
+        st.write("# Inference")
+        inference_time = st.number_input("Inference duration (hours)", min_value=0)
+        inference_device_type = st.selectbox("Inference device type", device_types)
+        inference_cpu_cores = st.slider("Inference number of CPU cores", 1, 32, 8)
+        inference_cpu_model = st.selectbox("Inference CPU model", cpu_models)
+        inference_gpu = st.slider("Inference number of GPUs", 1, 16, 8)
+        inference_gpu_model = st.selectbox("Inference GPU model", gpu_models)
+        inference_ram = st.slider("Inference RAM (GB)", 1, 64, 16)
+
+        st.write("# Cloud computing")
+        cloud_provider = st.selectbox("Cloud provider", cloud_providers.keys())
+        server_region = st.selectbox("Server region", server_regions)
+
+        submited = st.form_submit_button("Submit")
+    
+    if submited:
+        datacenter_efficiency = cloud_providers[cloud_provider]
+        datacenter_emission = server_regions[server_region]
+        training_cpu_power = cpu_models[training_cpu_model]
+        training_gpu_power = gpu_models[training_gpu_model]
+        inference_cpu_power = cpu_models[inference_cpu_model]
+        inference_gpu_power = gpu_models[inference_gpu_model]
+        power_per_gb_stored = 0.000001
+        power_per_ram_gb = 0.0003725
+        
+        total_training = training_cpu_cores*training_cpu_power if training_device_type in ("CPU", "Both") else 0
+        total_training += training_gpu*training_gpu_power if training_device_type in ("GPU", "Both") else 0
+        total_training += training_ram*power_per_ram_gb
+        total_training *= training_time
+        total_inference = inference_cpu_cores*inference_cpu_power if inference_device_type in ("CPU", "Both") else 0
+        total_inference += inference_gpu*inference_gpu_power if inference_device_type in ("GPU", "Both") else 0
+        total_inference += inference_ram*power_per_ram_gb
+        total_inference *= inference_time
+        total = total_training + total_inference
+        total += storing_time*power_per_gb_stored
+        total *= datacenter_efficiency*datacenter_emission/1000
+        
+        
+        
+        
+        st.write(f"""
+                 Data has been submited successfully.
+                 ### Total carbon footprint (kg of CO$_2$ equivalent):
+                 | {total:.5f} |
+                 | --- |
+                 """)
+    else:
+        st.write("Fill the form in the side bar to get an estimation of your CO2 emissions. If the precise answer is not available, choose the closes option.")
+        
+    
